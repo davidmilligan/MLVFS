7b23863f72fa1362f9e924032ff22ce2299975bb
David Milligan <david.l.milligan@gmail.com>
2014-08-26 23:15:58 -0400

    file attributes for the dng files (size does not include dng header size yet)

--- mlvfs/main.c
+++ mlvfs/main.c
@@ -29,6 +29,10 @@
 #include "raw.h"
 #include "mlv.h"
 
+#define DNG_THUMBNAIL_WIDTH     128
+#define DNG_THUMBNAIL_HEIGHT    84
+#define DNG_THUMBNAIL_SIZE      (DNG_THUMBNAIL_WIDTH * DNG_THUMBNAIL_HEIGHT * 3)
+
 struct mlvfs
 {
     char * mlv_path;
@@ -42,6 +46,71 @@ static int string_ends_with(const char *source, const char *ending)
     if(strlen(source) <= 0) return 0;
     if(strlen(source) < strlen(ending)) return 0;
     return !strcmp(source + strlen(source) - strlen(ending), ending);
+}
+
+static int get_mlv_filename(const char *path, char * mlv_filename)
+{
+    char temp[1024];
+    char *split;
+    strncpy(temp, path, 1024);
+    if((split = strrchr(temp, '/')) != NULL)
+    {
+        *split = 0x00;
+        sprintf(mlv_filename, "%s%s", mlvfs.mlv_path, temp);
+        return 1;
+    }
+    return 0;
+}
+
+static int dng_get_header_size(struct raw_info * raw_info)
+{
+    return 0;
+}
+
+static int dng_get_size(struct raw_info * raw_info)
+{
+    return dng_get_header_size(raw_info) + DNG_THUMBNAIL_SIZE + raw_info->frame_size;
+}
+
+static off_t mlv_get_dng_size(const char *path)
+{
+    off_t result = 0;
+    mlv_hdr_t mlv_hdr;
+    uint64_t position = 0;
+    FILE * mlv_file = fopen(path, "rb");
+    if(mlv_file)
+    {
+        position = ftell(mlv_file);
+        while(fread(&mlv_hdr, sizeof(mlv_hdr_t), 1, mlv_file))
+        {
+            fseek(mlv_file, position, SEEK_SET);
+            if(!memcmp(mlv_hdr.blockType, "RAWI", 4))
+            {
+                mlv_rawi_hdr_t rawi_hdr;
+                uint32_t hdr_size = sizeof(mlv_rawi_hdr_t) < mlv_hdr.blockSize ? sizeof(mlv_rawi_hdr_t) : mlv_hdr.blockSize;
+                
+                if(fread(&rawi_hdr, hdr_size, 1, mlv_file))
+                {
+                    result = dng_get_size(&rawi_hdr.raw_info);
+                    break;
+                }
+                else
+                {
+                    fprintf(stderr, "File ends in the middle of a block\n");
+                    break;
+                }
+            }
+            
+            fseek(mlv_file, position + mlv_hdr.blockSize, SEEK_SET);
+            position = ftell(mlv_file);
+        }
+        fclose(mlv_file);
+    }
+    else
+    {
+        fprintf(stderr, "Could not open file: '%s'\n", path);
+    }
+    return result;
 }
 
 static int mlv_get_frame_count(const char *path)
@@ -100,9 +169,25 @@ static int mlvfs_getattr(const char *path, struct stat *stbuf)
     
     if (string_ends_with(path, ".DNG"))
     {
-        stbuf->st_mode = S_IFREG | 0444;
-        stbuf->st_nlink = 1;
-        stbuf->st_size = 0;
+        char mlv_filename[1024];
+        if(get_mlv_filename(path, mlv_filename))
+        {
+            struct stat mlv_stat;
+            stbuf->st_mode = S_IFREG | 0444;
+            stbuf->st_nlink = 1;
+            stbuf->st_size = mlv_get_dng_size(mlv_filename);
+            if(stat(mlv_filename, &mlv_stat) == 0)
+            {
+                memcpy(&stbuf->st_atimespec, &mlv_stat.st_atimespec, sizeof(struct timespec));
+                memcpy(&stbuf->st_birthtimespec, &mlv_stat.st_birthtimespec, sizeof(struct timespec));
+                memcpy(&stbuf->st_ctimespec, &mlv_stat.st_ctimespec, sizeof(struct timespec));
+                memcpy(&stbuf->st_mtimespec, &mlv_stat.st_mtimespec, sizeof(struct timespec));
+            }
+        }
+        else
+        {
+            return -ENOENT;
+        }
     }
     else
     {
