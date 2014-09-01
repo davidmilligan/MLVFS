18570df6fb698ce13ce49ef16d929f831f2212c4
David Milligan <david.l.milligan@gmail.com>
2014-09-01 18:40:03 -0400

    cleanup bit unpacking and make it generic (should work with any source bit depth hopefully), still getting strips of bad image data though

--- mlvfs/dng.c
+++ mlvfs/dng.c
@@ -203,45 +203,26 @@ size_t dng_get_header_size(struct frame_headers * frame_headers)
 size_t dng_get_image_data(struct frame_headers * frame_headers, FILE * file, uint8_t * output_buffer, off_t offset, size_t max_size)
 {
     //unpack bits to 16 bit little endian (LSB first)
+    int bpp = frame_headers->rawi_hdr.raw_info.bits_per_pixel;
     uint64_t pixel_start_index = (size_t)MAX(0, offset) / 2; //lets hope offsets are always even for now
-    uint64_t pixel_start_address = pixel_start_index * 14 / 8;
+    uint64_t pixel_start_address = pixel_start_index * bpp / 16;
     size_t output_size = max_size - (offset < 0 ? (size_t)(-offset) : 0);
     uint64_t pixel_count = output_size / 2;
-    uint64_t packed_size = (pixel_count + 1) * 16 / 14;
-    uint8_t * packed_bits = malloc((size_t)packed_size);
+    uint64_t packed_size = (pixel_count + 1) * 16 / bpp;
+    uint16_t * packed_bits = malloc((size_t)packed_size);
     uint8_t * buffer = output_buffer + (offset < 0 ? (size_t)(-offset) : 0) + offset % 2;
     memset(buffer, 0, output_size);
     if(packed_bits)
     {
-        fseek(file, frame_headers->position + frame_headers->vidf_hdr.frameSpace + sizeof(mlv_vidf_hdr_t) + (size_t)pixel_start_address, SEEK_SET);
+        fseek(file, frame_headers->position + frame_headers->vidf_hdr.frameSpace + sizeof(mlv_vidf_hdr_t) + (size_t)pixel_start_address * 2, SEEK_SET);
         if(fread(packed_bits, (size_t)packed_size, 1, file))
         {
             for(size_t pixel_index = 0; pixel_index < pixel_count; pixel_index++)
             {
-                uint64_t pixel_address = (pixel_index + pixel_start_index) * 14 / 8;
-                uint64_t pixel_offset = (pixel_index + pixel_start_index) * 14 % 8;
-                uint64_t byte1_address = pixel_address + (pixel_address % 2 ? -1 : 1) - pixel_start_address;
-                uint64_t byte2_address = pixel_address + (pixel_address % 2 ? 2 : 0) - pixel_start_address;
-                uint64_t byte3_address = byte1_address + 2;
-                switch(pixel_offset)
-                {
-                    case 0:
-                        buffer[pixel_index * 2 + 1] = (packed_bits[byte1_address] >> 2) & 0x3F;
-                        buffer[pixel_index * 2] = ((packed_bits[byte1_address] << 6) & 0xC0) | ((packed_bits[byte2_address] >> 2) & 0x3F);
-                        break;
-                    case 2:
-                        buffer[pixel_index * 2 + 1] = packed_bits[byte1_address] & 0x3F;
-                        buffer[pixel_index * 2] = packed_bits[byte2_address];
-                        break;
-                    case 4:
-                        buffer[pixel_index * 2 + 1] = (((packed_bits[byte1_address] << 2) & 0x3C) | ((packed_bits[byte2_address] >> 6) & 0x03)) & 0x3F;
-                        buffer[pixel_index * 2] = ((packed_bits[byte2_address] << 2) & 0xFC) | ((packed_bits[byte3_address] >> 6) & 0x03);
-                        break;
-                    case 6:
-                        buffer[pixel_index * 2 + 1] = (((packed_bits[byte1_address] << 4) & 0x30) | ((packed_bits[byte2_address] >> 4) & 0x0F)) & 0x3F;
-                        buffer[pixel_index * 2] = ((packed_bits[byte2_address] << 4) & 0xF0) | ((packed_bits[byte3_address] >> 4) & 0x0F);
-                        break;
-                }
+                uint64_t pixel_address = (pixel_index + pixel_start_index) * bpp / 16 - pixel_start_address;
+                uint64_t pixel_offset = (pixel_index + pixel_start_index) * bpp % 16;
+                uint32_t data = ((packed_bits[pixel_address] << 16) & 0xFFFF0000) | (packed_bits[pixel_address + 1] & 0xFFFF);
+                *(uint16_t*)(buffer + pixel_index * 2) = (uint16_t)((data >> ((32 - bpp) - pixel_offset)) & 0x3FFF);
             }
         }
         else
@@ -255,12 +236,7 @@ size_t dng_get_image_data(struct frame_headers * frame_headers, FILE * file, uin
 
 size_t dng_get_image_size(struct frame_headers * frame_headers)
 {
-    if(frame_headers->rawi_hdr.raw_info.bits_per_pixel != 14)
-    {
-        fprintf(stderr, "Only 14-bit source data is supported");
-        return 0;
-    }
-    return (frame_headers->vidf_hdr.blockSize - frame_headers->vidf_hdr.frameSpace - sizeof(mlv_vidf_hdr_t)) * 16 / 14; //16 bit
+    return (frame_headers->vidf_hdr.blockSize - frame_headers->vidf_hdr.frameSpace - sizeof(mlv_vidf_hdr_t)) * 16 / frame_headers->rawi_hdr.raw_info.bits_per_pixel; //16 bit
 }
 
 size_t dng_get_size(struct frame_headers * frame_headers)
